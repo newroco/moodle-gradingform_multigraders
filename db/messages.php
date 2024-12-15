4c9f6b0b123781723bc67df299c791db8e3d2d4d
Alistair Spark <aspark21@users.noreply.github.com>
2024-12-15 10:24:46 +0000

    Fix #23

--- db/messages.php
+++ db/messages.php
@@ -28,7 +28,7 @@ $messageproviders = array (
 // Ordinary single forum posts
 'secondgrading' => array (
 'defaults' => [
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
+            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_ENABLED,
             'email' => MESSAGE_PERMITTED
         ],
 )
